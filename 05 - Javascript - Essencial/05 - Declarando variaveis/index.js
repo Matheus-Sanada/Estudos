7dfeb72806a3fd927cf54a5e59d13d086ea72bee
Matheus-Sanada <matheus.dev@outlook.com.br>
2022-03-21 20:01:16 -0300

    Exercicios

--- 05 - Javascript - Essencial/05 - Declarando variaveis/index.js
+++ 05 - Javascript - Essencial/05 - Declarando variaveis/index.js
@@ -6,3 +6,16 @@ console.log(spaceship)
 
 const rolezinhos = "Praia Grande"
 console.log(rolezinhos)
+
+var estudos = "javascript"
+console.log(estudos)
+
+//* Podemos definir uma let sem propriedade que no caso sera uma var undefined *//
+
+//* confirm e prompt -> Os dois retornam um valor
+
+let deportConfirmation = confirm("Podemos voar?")
+console.log(deportConfirmation)
+
+let pilot = prompt("Digite seu nome, piloto!")
+console.log(pilot)
---
+++ 05 - Javascript - Essencial/06 - Exercicio/index.md
@@ -0,0 +1,7 @@
+## PEGANDO DADOS DO USUARIO
+
+1. Exiba com um alerta a seguinte mensagem de boas-vindas ao usuario.
+2. Peça que o usuario informe seu nome.
+3. Peça que o usuario informe sua idade.
+4. Peça que ele confirme sua idade.
+5. Exiba um alerta com: O nome do usuario digitadom a idade e a confirmação da idade.
