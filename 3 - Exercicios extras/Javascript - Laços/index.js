0cecc906251ad497e0a72e4ec57f67a086cd5e97
Matheus-Sanada <matheus.dev@outlook.com.br>
2022-04-11 20:42:58 -0300

    update

---
+++ 3 - Exercicios extras/Javascript - Laços/index.js
@@ -0,0 +1,61 @@
+// let i = 0
+
+// while (i < 21) {
+
+//     console.log('5 x ' + i + ' = ' + 5*i);
+
+//     i++
+    
+// }
+
+//* A sintaxe do while, como vemos no exemplo acima, é bem simples. Utilizamos a palavra reservada while , logo em seguida precisamos passar uma condição que será atendida enquanto ela retornar true, ou seja, verdadeiro.
+
+//* Neste caso, simulamos a tabuada do 5, a variável i é o nosso contador, iniciamos ela com o valor 0 , portanto a condição será verdadeira até que ela atinja o valor 11, e por isso ela é iterada a cada repetição - i++, ao atingir tal valor o laço é quebrado.
+
+
+
+
+//* Crie um programa que imprima os numeros de 0 a 100
+
+// let i = 0
+
+// while (i < 100) {
+
+//     i ++ 
+//     console.log(`A contagem esta em ${i}`)
+    
+// }
+
+
+//* Crie um programa que imprima os numeros de 0 a 100 apenas com os numeros pares.
+
+// let i = 0
+// let par = 2
+
+// while (i < 100) {
+
+//     i += par
+//     console.log(`A contagem esta em ${i}`)
+    
+// }
+
+//* Elaborar um programa que apresente os resultados de uma tabuada de um numero inteiro qualquer fornecido. A tabuada deve seguir o padrão estando entre 1 e 10.
+
+// let numOne = prompt("Digite o primeiro numero: ")
+// let numTwo = prompt("Digite o segundo numero: ")
+
+// let result = 0
+
+// while (result < 11) {
+
+//     result = numOne * numTwo
+
+//     alert("O resultado da sua operação é " +  numOne  + "X" +  numTwo  + "=" + result)
+
+//     result ++
+
+    
+    
+// }
+
+
---
+++ 3 - Exercicios extras/Javascript - Laços/index.md
@@ -0,0 +1,24 @@
+
+2. Leia 20 valores reais e calcule seu somatório utilizando a instrução do...while.
+3. Refaça o exercício abaixo (agora em javascript), mas desta vez utilizando as estruturas
+de repetição, onde o programa deverá finalizar quando o usuário digitar 0, caso
+contrário ficará cadastrando os pedidos, agora vai poder acumular mais de um produto
+e ao final deverá ser dada a soma dos pedidos.
+O cardápio de uma casa de lanches é dado pela tabela abaixo:
+Código Produto Preço Unitário (R$)
+100 Cachorro quente R$ 1,70
+101 Bauru Simples R$ 2,30
+102 Bauru com ovo R$ 2,60
+103 Hamburguer R$ 2,40
+104 Cheeseburguer R$ 2,50
+105 Refrigerante R$ 1,00
+Escreva um algoritmo que leia o código do item adquirido pelo consumidor e a
+quantidade, calculando e mostrando o valor a pagar. Não será necessário exibir o
+produto e o valor, somente o valor final.
+4. Faça um algoritmo que leia um conjunto de 15 valores, um de cada vez,
+acompanhados de um código 1 ou 2. O valor representa o número de cobaias utilizadas
+em uma das 15 experiências feitas e os códigos 1 e 2 representam respectivamente
+coelhos e ratos. Mostre no final, o total de cobaias utilizadas, o total de coelhos, total
+de ratos, e o percentual de coelhos e ratos.
+5. Faça um algoritmo que apresente o quadrado de cada um dos números pares entre 1
+a 100.
