25e9bb6e6692b8f656be92cf2975af80da26eebe
Matheus-Sanada <matheus.dev@outlook.com.br>
2022-04-11 20:01:34 -0300

    Estudos

---
+++ 2 - Curso FullStack 2.0/05 - Javascript - Essencial/032 - Arrow functions/index.js
@@ -0,0 +1,5 @@
+//* Arrow functions -> É o terceiro tipo de função e as Arrow functions tambem é uma função anonima.
+
+const doubleSpeed = (velocity) => velocity * 4
+
+console.log(doubleSpeed(30))
