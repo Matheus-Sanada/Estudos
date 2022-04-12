2b8e18c389c5d9a184ab56833729ac02b6f24bb5
Matheus-Sanada <matheus.dev@outlook.com.br>
2022-04-12 12:30:21 -0300

    Exercicios

--- 3 - Exercicios extras/Javascript - Laços/index.js
+++ 3 - Exercicios extras/Javascript - Laços/index.js
@@ -58,4 +58,100 @@
     
 // }
 
+//* Crie um programa que mostra os anos de 2022 para frente.
+
+// let year = 2022
+
+// let years = 1
+
+// while (year <= 2100) {
+
+//     year += years
+
+//     console.log("Você esta no ano de" + year)
+// }
+
+
+//* Crie um programa que mostra o ano do seu nascimento
+
+// let yearUser = prompt("Digite o ano em que vocè esta: ")
+// let date = prompt("Digite a sua idade: ")
+
+// let result = yearUser - date
+
+// while (result) {
+
+//     result = result
+
+//     if (result >= 1960) {
+
+//         confirm(`Parabens vocè nasceu no ano de ${result}`)
+        
+//     } else {
+
+//         alert("Eai mummiaaaaaaaaaaaaaa, vocè nasceu no ano dos sarcofagos rsrsr ano de" + result)
+
+//     }
+
+
+//     break;
+    
+// }
+
+// alert("Encerrando programa:)")
+
+
+//* Crie um programa que abra um serie de opções quando você add sua idade.
+
+alert("Seja bem vindo(a)")
+
+let navBar = prompt("O que você deseja fazer agora?\n\n1 - Saber o ano do meu nascimento\n2 - Saber a idade de outra pessoa")
+
+let year = 2022
+
+let nameUser
+let result 
+let yearMoment
+let age 
+
+while (year <= 2022) {
+
+    year = year
+
+
+        switch (navBar) {
+
+            case "1":
+
+            nameUser = prompt("Digite o seu nome: ")
+            age = prompt("Digite a sua idade: ")
+            result = year - age
+
+            alert(`Parabens ${nameUser}, você tem a idade de ${age} anos e o ano do seu nascimento é ${result}`)
+                
+                break;
+
+            case "2":
+
+            nameUser = prompt("Digite o nome da pessoa: ")
+            yearMoment = prompt("Digite a idade da pessoa que você quer descobrir: ")
+            result = year - yearMoment
+
+            alert(`Parabens ${nameUser}, você tem a idade de ${yearMoment} anos e o ano do seu nascimento é ${result}`)
+                
+                break;
+        
+            default:
+
+            alert("Numero invalido")
+
+                break;
+        }
+
+    break;
+
+}
+
+alert("Encerrando programa :(")
+
 
