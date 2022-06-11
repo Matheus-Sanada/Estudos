dc835a7e94c2cdb6eba9307249f8eb536d5991b8
Matheus-Sanada <matheus.dev@outlook.com.br>
2022-06-11 18:24:40 -0300

    Curso novo

---
+++ 2 - Curso web moderno 2022/Javascript/1 - Javascript essencial/07 - Template string/index.js
@@ -0,0 +1,15 @@
+const name = "Matheus Sanada"
+const concat = "Ola " + name + " Seja bem-vindo(a)"
+console.log(concat)
+
+
+
+
+//* TEMPLATE STRINGS *//
+
+
+let template = "Matheus Sanada"
+let age = 22
+let states = "São paulo"
+
+console.log(`O ${template} que tem a idade de ${age} anos, veio do estado de ${states}.`)
---
+++ 2 - Curso web moderno 2022/Javascript/1 - Javascript essencial/08 - Boolean/index.js
@@ -0,0 +1,9 @@
+let name = false
+console.log(name)
+
+let age = true
+console.log(age)
+
+let num = 1
+console.log(!!num)
+// Para converte algo em verdadeiro ou falso usamos os simbolo de negação(!) => falso e (!!) => verdadeiro
